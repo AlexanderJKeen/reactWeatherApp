b0be3e1057afcc93ed5652215e3fd5a64766d12b
Alexander Keen <alexander.keen@keenkom.co.uk>
2020-03-16 13:40:39 +0000

    Updates

---
+++ precache-manifest.a9b5e25f527dce192463f78c52749210.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d812507fdb40b685b2640e79ee3ef227",
+    "url": "/reactWeatherApp/index.html"
+  },
+  {
+    "revision": "98f17b819d058fd10a32",
+    "url": "/reactWeatherApp/static/css/main.5ba5976e.chunk.css"
+  },
+  {
+    "revision": "711986f8b83d46ade94a",
+    "url": "/reactWeatherApp/static/js/2.97e49b8a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactWeatherApp/static/js/2.97e49b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f17b819d058fd10a32",
+    "url": "/reactWeatherApp/static/js/main.af17ec1b.chunk.js"
+  },
+  {
+    "revision": "b2b1e01c115224491044",
+    "url": "/reactWeatherApp/static/js/runtime-main.5634e793.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/reactWeatherApp/static/media/cold.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/reactWeatherApp/static/media/warm.635ed87d.jpg"
+  }
+]);
